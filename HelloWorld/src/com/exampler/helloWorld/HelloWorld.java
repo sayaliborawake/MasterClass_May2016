722c3fe366e7fc683b1610b28a4f228f2f7804f2
sayali <borawakesayali@gmail.com>
2016-05-12 23:45:17 +0530

    commit for master class

---
+++ HelloWorld/src/com/exampler/helloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.exampler.helloWorld;
+
+/**
+ * Created by sayali on 12/05/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ HelloWorld/test/com/exampler/helloWorld/HelloWorldTest.groovy
@@ -0,0 +1,12 @@
+package com.exampler.helloWorld
+
+import junit.framework.TestCase
+
+/**
+ * Created by sayali on 12/05/2016.
+ */
+class HelloWorldTest extends TestCase {
+    void testMain() {
+
+    }
+}
